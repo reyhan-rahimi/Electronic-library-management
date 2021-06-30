93d2c50c19262285b078a111d7fff8bd606b652d
reyhane <afsonrahimi1378@gmail.com>
2021-07-01 00:29:39 +0430

    add interfaces

---
+++ CODE/interface/ICUDlibrarian.cs
@@ -0,0 +1,21 @@
+﻿using LibraryUnivercity.entity;
+using LibraryUnivercity.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryUnivercity.interfaces
+{
+    public interface ICUDlibrarian
+    {
+        public User CreateAccount(User user);
+        public Books AddBook(Books book);
+        public bool DeleteAccount(UserModel userModel);
+        public bool DeleteBook(BookModel bookModel);
+        public bool BorrowBook(UserModel userModel, BookModel bookModel);
+        public bool ReturnBook(UserModel userModel, BookModel bookModel);
+        public bool UpdateAccount(UserModel userModel);
+        public bool CreateOrder(BookModel bookModel);
+    }
+}
---
+++ CODE/interface/ICUDuser.cs
@@ -0,0 +1,15 @@
+﻿using LibraryUnivercity.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryUnivercity.interfaces
+{
+    public interface ICUDuser
+    {
+        public BookModel SearchBook(BookModel bookModel);
+        public BookModel ShowBorrowList(UserModel userModel);
+        public bool AddTime(UserModel userModel, BookModel bookModel);
+    }
+}
