3b76d8fc58dee64a1377d93da8cb272b86e93e75
reyhane <afsonrahimi1378@gmail.com>
2021-07-02 22:38:03 +0430

    final update startup file in back end

---
+++ CODE/Startup.cs
@@ -0,0 +1,72 @@
+using LibraryUnivercity.entity;
+using LibraryUnivercity.interfaces;
+using LibraryUnivercity.repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LibraryUnivercity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            
+            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build(); 
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            var conection = Configuration.GetConnectionString("services");
+            services.AddDbContext<DataBase>(option => option.UseNpgsql(conection));
+            services.AddTransient<ICUDbook, CUDbook>()
+            services.AddControllers();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
